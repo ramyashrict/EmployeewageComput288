e115ed49455823d6b0b6c3e8dc5b11c1b34be4e8
ramyashrict <ramyarr1415@gmail.com>
2023-06-20 04:20:52 +0530

    added

--- Program.cs
+++ Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Employeewagecomput288
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("WEL COME");
         }
     }
 }
